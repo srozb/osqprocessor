831dd9685958f8b5a0d18814da8d05431955a5d0
srozb <github@rozbicki.eu>
2017-07-13 15:02:29 +0200

    daemon support imporved

--- config.py
+++ config.py
@@ -3,5 +3,6 @@
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 REDIS_DB = 0
-DAEMON = False
-LOG_LEVEL = "DEBUG"
+DAEMON = True
+LOG_LEVEL = 'DEBUG'
+SYSLOG_ADDR = '/dev/log'
--- logger/logger.py
+++ logger/logger.py
@@ -1,13 +1,20 @@
 import logging
+import logging.handlers
 import config
 
 class Logger(object):
     console_log_format = "%(asctime)-15s %(name)s %(message)s"
+    syslog_log_format = "osqprocessor[%(process)d]: %(name)s %(message)s"
     def __init__(self, mod_name):
         self.logger = logging.getLogger(mod_name)
         self.logger.setLevel(config.LOG_LEVEL)
-        self.formatter = logging.Formatter(self.console_log_format)
-        self.handler = logging.StreamHandler()
+
+        if config.DAEMON:
+            self.formatter = logging.Formatter(self.syslog_log_format)
+            self.handler = logging.handlers.SysLogHandler(address = config.SYSLOG_ADDR)
+        else:
+            self.formatter = logging.Formatter(self.console_log_format)
+            self.handler = logging.StreamHandler()
         self.handler.setFormatter(self.formatter)
         self.logger.addHandler(self.handler)
 
